3bf00171b30c0dd2ede4c5776fc10449f5741026
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-10-27 20:52:10 -0500

    npm install/ input connection in server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const PORT = 3306;
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: PORT,
+    user: "root",
+    password: "L@ur@1990",
+    database: "employeeTrakerDB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Listening on port " + PORT)
+    start();
+})
